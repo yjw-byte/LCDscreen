37b59128840be8e48206e6dd796f12ff3cca8a8d
86151 <3226316096@qq.com>
2025-11-01 16:17:13 +0800

    添加了屏幕分辨率定义： 修改了显示刷新函数dsp_flush，使用您提供的LCD驱动函数： 使用LCD_SetWindows设置显示区域 使用LCD_WR_DATA_16发送像素数据 保留了lv_disp_flush_ready(disp_drv)确保LVGL知道刷新完成 移除了示例中不相关的缓冲区配置，只保留了一个简单的单缓冲区配置。
    Took 5 minutes

--- lvgl-8.0.2-core/examples/porting/lv_port_disp.c
+++ lvgl-8.0.2-core/examples/porting/lv_port_disp.c
@@ -11,10 +11,13 @@
  *********************/
 #include "lv_port_disp.h"
 #include "../../lvgl.h"
+#include "lcd.h"
 
 /*********************
  *      DEFINES
  *********************/
+#define MY_DISP_HOR_RES 128
+#define MY_DISP_VER_RES 160
 
 /**********************
  *      TYPEDEFS
@@ -78,18 +81,6 @@ void lv_port_disp_init(void)
     static lv_color_t buf_1[MY_DISP_HOR_RES * 10];                          /*A buffer for 10 rows*/
     lv_disp_draw_buf_init(&draw_buf_dsc_1, buf_1, NULL, MY_DISP_HOR_RES * 10);   /*Initialize the display buffer*/
 
-    /* Example for 2) */
-    static lv_disp_draw_buf_t draw_buf_dsc_2;
-    static lv_color_t buf_2_1[MY_DISP_HOR_RES * 10];                        /*A buffer for 10 rows*/
-    static lv_color_t buf_2_1[MY_DISP_HOR_RES * 10];                        /*An other buffer for 10 rows*/
-    lv_disp_draw_buf_init(&draw_buf_dsc_2, buf_2_1, buf_2_1, MY_DISP_HOR_RES * 10);   /*Initialize the display buffer*/
-
-    /* Example for 3) also set disp_drv.full_refresh = 1 below*/
-    static lv_disp_draw_buf_t draw_buf_dsc_3;
-    static lv_color_t buf_3_1[MY_DISP_HOR_RES * MY_DISP_VER_RES];            /*A screen sized buffer*/
-    static lv_color_t buf_3_1[MY_DISP_HOR_RES * MY_DISP_VER_RES];            /*An other screen sized buffer*/
-    lv_disp_draw_buf_init(&draw_buf_dsc_3, buf_3_1, buf_3_2, MY_DISP_VER_RES * LV_VER_RES_MAX);   /*Initialize the display buffer*/
-
     /*-----------------------------------
      * Register the display in LVGL
      *----------------------------------*/
@@ -100,22 +91,14 @@ void lv_port_disp_init(void)
     /*Set up the functions to access to your display*/
 
     /*Set the resolution of the display*/
-    disp_drv.hor_res = 480;
-    disp_drv.ver_res = 320;
+    disp_drv.hor_res = MY_DISP_HOR_RES;
+    disp_drv.ver_res = MY_DISP_VER_RES;
 
     /*Used to copy the buffer's content to the display*/
     disp_drv.flush_cb = disp_flush;
 
     /*Set a display buffer*/
     disp_drv.draw_buf = &draw_buf_dsc_1;
-
-    /*Required for Example 3)*/
-    //disp_drv.full_refresh = 1
-
-    /* Fill a memory array with a color if you have GPU.
-     * Note that, in lv_conf.h you can enable GPUs that has built-in support in LVGL.
-     * But if you have a different GPU you can use with this callback.*/
-    //disp_drv.gpu_fill_cb = gpu_fill;
 
     /*Finally register the driver*/
     lv_disp_drv_register(&disp_drv);
@@ -128,6 +111,7 @@ void lv_port_disp_init(void)
 /*Initialize your display and the required peripherals.*/
 static void disp_init(void)
 {
+    LCD_Init_HAL();
     /*You code here*/
 }
 
@@ -136,14 +120,16 @@ static void disp_init(void)
  *'lv_disp_flush_ready()' has to be called when finished.*/
 static void disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
 {
-    /*The most simple case (but also the slowest) to put all pixels to the screen one-by-one*/
-
-    int32_t x;
-    int32_t y;
+    int32_t x, y;
+    
+    // 设置显示窗口
+    LCD_SetWindows(area->x1, area->y1, area->x2, area->y2);
+    
+    // 逐个发送像素数据
     for(y = area->y1; y <= area->y2; y++) {
         for(x = area->x1; x <= area->x2; x++) {
-            /*Put a pixel to the display. For example:*/
-            /*put_px(x, y, *color_p)*/
+            // 发送像素颜色数据，注意LVGL使用的是RGB565格式
+            LCD_WR_DATA_16(color_p->full);
             color_p++;
         }
     }
